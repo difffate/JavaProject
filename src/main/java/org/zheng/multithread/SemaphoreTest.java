583933fe958e1320a8f4a40001a5002dbde7229d
difffate <difffate@163.com>
2017-05-06 00:52:56 +0800

    添加注释

--- src/main/java/org/zheng/multithread/SemaphoreTest.java
+++ src/main/java/org/zheng/multithread/SemaphoreTest.java
@@ -13,11 +13,16 @@ public class SemaphoreTest {
 
     private static final int THREAD_COUNT = 5;
 
-    private static ExecutorService threadPool = Executors
-            .newFixedThreadPool(THREAD_COUNT);
+    private static ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_COUNT);
 
     private static Semaphore s = new Semaphore(3);
 
+    /**
+     * tryAcquire()
+     * 尝试获取permits个许可，成功则返回true，失败则返回false
+     *
+     * @throws InterruptedException
+     */
     public static void testTryAcquire1() throws InterruptedException {
         if (s.tryAcquire()) {  //尝试获取一个许可
             System.out.println(Thread.currentThread().getName() + " 获得许可");
@@ -29,6 +34,12 @@ public class SemaphoreTest {
         }
     }
 
+    /**
+     * tryAcquire(int permits)
+     * 尝试获取permits个许可，成功则返回true，失败则返回false
+     *
+     * @throws InterruptedException
+     */
     public static void testTryAcquire2() throws InterruptedException {
         if (s.tryAcquire(1)) {  //尝试获取一个许可
             System.out.println(Thread.currentThread().getName() + " 获得许可");
@@ -40,6 +51,12 @@ public class SemaphoreTest {
         }
     }
 
+    /**
+     * tryAcquire(long timeout, TimeUnit unit)
+     * 设置超时时间内能获取到1个许可都会返回true，否则返回false
+     *
+     * @throws InterruptedException
+     */
     public static void testTryAcquire3() throws InterruptedException {
         if (s.tryAcquire(1, TimeUnit.SECONDS)) {   //尝试获取一个许可，超时时间1s
             System.out.println(Thread.currentThread().getName() + " 获得许可");
@@ -51,12 +68,34 @@ public class SemaphoreTest {
         }
     }
 
+    /**
+     * 不断尝试，直到都获取到信号量执行任务
+     *
+     * @throws InterruptedException
+     */
+    public static void testTryAcquire4() throws InterruptedException {
+        while (true) {
+            if (s.tryAcquire(1, TimeUnit.SECONDS)) {
+                System.out.println(Thread.currentThread().getName() + " 获得许可");
+                Thread.sleep(3000);
+                System.out.println(Thread.currentThread().getName() + " 操作完毕");
+                s.release();   //释放许可证
+                break;
+            }
+        }
+    }
+
+    /**
+     * 常规用法，先获取，获取不到则阻塞线程执行，获取到执行完，释放信号量
+     *
+     * @throws InterruptedException
+     */
     public static void testAcquire() throws InterruptedException {
-        s.acquire();
+        s.acquire();    //获取许可
         System.out.println(Thread.currentThread().getName() + " 获得许可");
-        Thread.sleep(2000);
+        Thread.sleep(1000);
         System.out.println(Thread.currentThread().getName() + " 操作完毕");
-        s.release();   //释放许可证
+        s.release();   //释放许可，只需要释放一次，释放多次会让信号量多增加
     }
 
     public static void main(String[] args) {
@@ -69,6 +108,7 @@ public class SemaphoreTest {
 //                        testTryAcquire1();
 //                        testTryAcquire2();
 //                        testTryAcquire3();
+//                        testTryAcquire4();
                         testAcquire();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
