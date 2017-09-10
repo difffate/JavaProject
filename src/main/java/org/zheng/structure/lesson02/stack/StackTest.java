523d93e59899bd555d6ab7d85587a924c2ba9819
zhengxb <zhengxb@101.com>
2017-09-10 20:26:58 +0800

    Test程序

--- src/main/java/org/zheng/structure/lesson02/stack/StackTest.java
+++ src/main/java/org/zheng/structure/lesson02/stack/StackTest.java
@@ -7,10 +7,12 @@ public class StackTest {
 
     public static void main(String[] args) {
         IStack stack = new StackImpl();
-        stack.push(4);
+        stack.push(1);
+        stack.push(2);
         stack.push(3);
+        stack.push(4);
         stack.pop();
         stack.pop();
-        System.out.println(stack.top());
+        System.out.println("Top：" + stack.top());
     }
 }
